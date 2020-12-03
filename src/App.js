3657fe61c360ffe350ff8c7f779baac659303eff
Jeff <jlui820@gmail.com>
2020-12-03 12:30:14 -0800

    add dropdown and add material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <h1>COVID-19 TRACKER</h1>
+
+      <FormControl className='app__dropdown'>
+        <Select
+          variant='outlined'
+          value='abc'
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
+          <MenuItem value='worldwide'>yooo</MenuItem>
+          <MenuItem value='worldwide'>a</MenuItem>
+        </Select>
+      </FormControl>
+
+      {/* Header */}
+      {/* title + select input drop down */}
+
+      {/* infoboxes */}
+      {/* infoboxes */}
+      {/* infoboxes */}
+
+      {/* table */}
+      {/* graph */}
+
+      {/* map */}
     </div>
   );
 }

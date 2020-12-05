747762b5abc4012a6a7fd463572a9a521a1073b4
Jeff <jlui820@gmail.com>
2020-12-05 11:20:13 -0800

    udpate dropdown menu so country code remains after click

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide')
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -26,25 +27,24 @@ function App() {
     };
 
     getCountriesData()
-  }, [])
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  } 
 
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className='app__dropdown'>
-          <Select
-            variant='outlined'
-            value='abc'
-          >
-
-            {
-              countries.map((country) => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
-            
-          </Select>
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+            </Select>
         </FormControl>
       </div>
 

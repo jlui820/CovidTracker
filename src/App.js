4f1a58211d5ed64a6f4c9b12941789cc5a3b15b5
Jeff <jlui820@gmail.com>
2020-12-14 16:05:29 -0800

    split main page layout in app-left and app-right

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import InfoBox from './InfoBox';
+import Map from './Map'
 import './App.css';
 import {
   MenuItem,
@@ -37,29 +38,35 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className='app__dropdown'>
-          <Select variant="outlined" value={country} onChange={onCountryChange}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-            </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className='app__dropdown'>
+            <Select variant="outlined" value={country} onChange={onCountryChange}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+              </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title='Coronavirus cases' cases={1000} total={2000}/>
+          <InfoBox title='Recovered' cases={1000} total={2000}/>
+          <InfoBox title='Deaths' cases={1000} total={2000}/>
+        </div>
+
+        <Map />
       </div>
-
-      <div className="app__stats">
-        <InfoBox title='Coronavirus cases' cases={1000} total={2000}/>
-        <InfoBox title='Recovered' cases={1000} total={2000}/>
-        <InfoBox title='Deaths' cases={1000} total={2000}/>
-      </div>
-
-
-      {/* table */}
-      {/* graph */}
-
-      {/* map */}
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          {/* table */}
+          <h3>Worldwide new cases</h3>
+          {/* graph */}
+        </CardContent>
+      </Card>
     </div>
   );
 }

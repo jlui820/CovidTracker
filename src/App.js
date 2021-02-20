9bcefa76eef941e17be5ec28173bb5755331fccf
Jeff <jlui820@gmail.com>
2021-02-19 16:07:02 -0800

    import chartJs2

--- src/App.js
+++ src/App.js
@@ -91,6 +91,7 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
+          <LineGraph />
           {/* graph */}
         </CardContent>
       </Card>
